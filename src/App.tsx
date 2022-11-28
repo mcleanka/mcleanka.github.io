76102a3fa9dfc778ef31c08db95f11b4271b1b66
mcleanka <mcleankasambala@gmail.com>
2022-11-28 22:45:27 +0200

    added about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import Sidebar from './components/Sidebar';
+import Container from 'react-bootstrap/Container';
+import About from './pages/About';
+import Experience from './pages/Experience';
+import Education from './pages/Education';
+import Skills from './pages/Skills';
+import Awards from './pages/Awards';
+import Interests from './pages/Interests';
+import './App.scss'
 
 export default function App() {
 	return (
 		<>
 			<Sidebar />
+			<Container className='p-0'>
+				<About />
+				<hr className='m-0' />
+				<Experience />
+				<hr className='m-0' />
+				<Education />
+				<hr className='m-0' />
+				<Skills />
+				<hr className='m-0' />
+				<Interests />
+				<hr className='m-0' />
+				<Awards />
+			</Container>
 		</>
 	)
 }
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,35 @@
 import React from 'react'
+import { FaLinkedinIn, FaGithub, FaTwitter, FaFacebookF } from "react-icons/fa";
+
+import './About.scss'
 
 export default function About() {
+	const icons: React.ReactElement[] = [<FaLinkedinIn />, <FaGithub />, <FaTwitter />, <FaFacebookF />]
+
 	return (
-		<div>About</div>
+		<>
+			<section className="resume-section" id="about">
+				<div className="resume-section-content">
+					<h1 className="mb-0">
+						{'Mclean'}{' '}
+						<span className="text-primary">{'Kasambala'}</span>
+					</h1>
+					<div className="subheading mb-5">
+						Malawi University of Business and Applied Sciences · Box 303, Chichiri Blantyre 3, Malawi  <br /> (+265) 99-159-3543 · <a href="mailto:mcleankasambala@gmail.com">mcleankasambala@gmail.com</a>
+					</div>
+					<p className="lead mb-5">
+						Motivated and results-driven novice computer engineer with a proven record of
+						accomplishment in IT (Information, and Technology) consultancy, hardware and software architecting, development, and deployment, and interested in developing valuable solutions to drive accuracy and process efficiency based on data findings.
+					</p>
+					<div className="social-icons">
+						{
+							icons.map((icon) => (<a className="social-icon bg-primary" href="#!">
+								{icon}
+							</a>))
+						}
+					</div>
+				</div>
+			</section>
+		</>
 	)
 }

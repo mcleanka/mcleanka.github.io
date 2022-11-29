381e5848ad5dbd04815d8f14ca0ac457e56d878e
mcleanka <mcleankasambala@gmail.com>
2022-11-29 09:55:48 +0200

    remove styles

--- src/pages/Awards.tsx
+++ src/pages/Awards.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { FaArrowRight } from 'react-icons/fa';
-import './Awards.scss';
 
 function Awards() {
 	return (
 		<section className="resume-section" id="awards">
 			<div className="resume-section-content">
 				<h2 className="mb-5">{'Awards & Certifications'}</h2>
-				<ul className="fa-ul mb-0">
+				<ul className="ml-0 mb-0">
 					<li>
 						<span className="fa-li">
 							<FaArrowRight />

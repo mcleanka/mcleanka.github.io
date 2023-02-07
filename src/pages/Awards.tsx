706ab89fdb3db9bed6b2d799d907d74a470bf48a
Mclean Kasambala <mcleankasambala@gmail.com>
2023-02-07 08:18:23 +0200

    Added awards page

--- src/pages/Awards.tsx
+++ src/pages/Awards.tsx
@@ -1,17 +1,33 @@
-import React from 'react';
-import { FaArrowRight } from 'react-icons/fa';
+import React, { FC, ReactElement, useEffect, useState } from 'react';
+import { NotFound } from '../components';
+import { award } from '../services';
+import { IAwards } from '../types/Award.type';
 
-function Awards() {
+const Awards: FC<{}> = (): ReactElement => {
+	const [awards, setAwards] = useState<IAwards[]>([])
+
+	async function getAwards(): Promise<void> {
+		await award.list('/awards', {})
+			.then<void, never>((result) => setAwards(result))
+	}
+
+	useEffect(() => {
+		getAwards()
+	}, [])
+
 	return (
 		<section className="resume-section" id="awards">
 			<div className="resume-section-content">
 				<h2 className="mb-5">{'Awards & Certifications'}</h2>
 				<ul className="ml-0 mb-0">
 					<li>
-						<span className="fa-li">
-							<FaArrowRight />
-						</span> {' '}
-						Coming soon...
+						{
+							(awards.length > 0) && <div></div>
+						}
+
+						{
+							!awards.length && <NotFound title='No award(s) or certification(s) found' />
+						}
 					</li>
 				</ul>
 			</div>
---
+++ src/pages/index.ts
@@ -0,0 +1,17 @@
+import About from './About';
+import Experience from './Experience';
+import Education from './Education';
+import Skills from './Skills';
+import Awards from './Awards';
+import Repositories from './Repositories';
+import Interests from './Interests';
+
+export {
+	About,
+	Experience,
+	Education,
+	Skills,
+	Awards,
+	Repositories,
+	Interests,
+};

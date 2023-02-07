6ac8ccbb391e8a7f92f0b6818ec3803d39c0c411
Mclean Kasambala <mcleankasambala@gmail.com>
2023-02-07 08:18:02 +0200

    file ext refactoring

--- src/services/index.tsx
+++
@@ -1,9 +0,0 @@
-import { repos } from "./repository.service"
-import { experience } from "./experience.service"
-import { education } from "./education.service"
-
-export {
-	repos,
-	experience,
-	education
-}

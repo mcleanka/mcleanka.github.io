0e37b93490d816f408404956f1f5def7ead40fc9
Mclean Kasambala <mcleankasambala@gmail.com>
2023-02-07 08:17:02 +0200

    file ext refactoring

---
+++ src/services/index.ts
@@ -0,0 +1,11 @@
+import { repos } from "./repository.service"
+import { experience } from "./experience.service"
+import { education } from "./education.service"
+import { award } from "./award.service"
+
+export {
+	repos,
+	experience,
+	education,
+	award,
+}

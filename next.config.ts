9b5942f9e6cebb9563090e2662102b721e1f80c6
Mclean Kasambala <mcleankasambala@gmail.com>
2024-11-04 15:06:36 +0200

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	output: 'export',
+	images: {
+		unoptimized: true,
+	},
+	basePath: process.env.NODE_ENV === 'production' ? '/mclean-profile/' : '',
+	assetPrefix: process.env.NODE_ENV === 'production' ? '/mclean-profile/' : '',
 };
 
 export default nextConfig;

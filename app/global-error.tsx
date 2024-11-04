50c4b2b2645bf21b410ba7286534a00b9e89998d
Mclean Kasambala <mcleankasambala@gmail.com>
2024-11-05 01:07:39 +0200

    added fonts

---
+++ app/global-error.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import NextError from "next/error";
+
+export default function GlobalError({ error }: { error: Error & { digest?: string } }) {
+
+	console.error(error)
+
+	return (
+		<html>
+			<body>
+				<NextError statusCode={0} />
+			</body>
+		</html>
+	);
+}

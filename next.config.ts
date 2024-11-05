b1ecf7b74d972ab1e8e51077ab9b3951058a7e1e
Mclean Kasambala <mcleankasambala@gmail.com>
2024-11-05 02:33:43 +0200

    path fixing

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
 	output: 'export',
 	images: {
 		unoptimized: true,
+		loader: 'custom',
+		loaderFile: './app/image-loader.js',
 	},
 	basePath: process.env.NODE_ENV === 'production' ? '/my-profile' : '',
 	assetPrefix: process.env.NODE_ENV === 'production' ? '/my-profile' : '',

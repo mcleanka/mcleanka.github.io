9397d60e223bd8015f813ccc1d521545c62c0956
Mclean Kasambala <mcleankasambala@gmail.com>
2024-11-05 02:04:10 +0200

    layout update

--- components/ui/grid-globe.tsx
+++ components/ui/grid-globe.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React from "react";
-import { motion } from "framer-motion";
 import dynamic from "next/dynamic";
 
 const World = dynamic(() => import("../ui/globe").then((m) => m.World), {
@@ -395,44 +394,6 @@ export function GridGlobe() {
 	];
 
 	return (
-		<div className="flex flex-row items-center justify-center py-20 h-screen md:h-auto dark:bg-black bg-white relative w-full">
-			<div className="max-w-7xl mx-auto w-full relative overflow-hidden h-full md:h-[40rem] px-4">
-				<motion.div
-					initial={{
-						opacity: 0,
-						y: 20,
-					}}
-					animate={{
-						opacity: 1,
-						y: 0,
-					}}
-					transition={{
-						duration: 1,
-					}}
-					className="div"
-				>
-					<h2 className="text-center text-xl md:text-4xl font-bold text-black dark:text-white">
-						We sell soap worldwide
-					</h2>
-					<p className="text-center text-base md:text-lg font-normal text-neutral-700 dark:text-neutral-200 max-w-md mt-2 mx-auto">
-						This globe is interactive and customizable. Have fun with it, and
-						don&apos;t forget to share it. :)
-					</p>
-				</motion.div>
-				<div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent dark:to-black to-white z-40" />
-				<div className="absolute w-full -bottom-20 h-72 md:h-full z-10">
-					<World data={sampleArcs} globeConfig={globeConfig} />
-				</div>
-			</div>
-		</div>
-	);
-}
-
-export default GridGlobe;
-
-/* const GridGlobe = () => {
-
-	return (
 		<div className="flex items-center justify-center absolute -left-5 top-36 md:top-40 w-full h-full">
 			<div className="max-w-7xl mx-auto w-full relative overflow-hidden h-96 px-4">
 				<div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent dark:to-black to-white z-40" />
@@ -442,6 +403,7 @@ export default GridGlobe;
 			</div>
 		</div>
 	);
-};
+}
 
- */
+export default GridGlobe;
+

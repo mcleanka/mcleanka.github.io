3a7dc032d4127d0f37fe3a9e34e63f64c1fe14a8
mcleanka <mcleankasambala@gmail.com>
2025-09-01 19:00:41 +0200

    deployment

--- components/sections/ContactSection.tsx
+++ components/sections/ContactSection.tsx
@@ -26,9 +26,7 @@ const ContactSection: React.FC<ContactSectionProps> = ({
                 </h3>
 
                 <p className="text-lg text-secondary mb-12 leading-relaxed">
-                    Although I&apos;m not currently looking for any new opportunities, my inbox
-                    is always open. Whether you have a question or just want to say hi,
-                    I&apos;ll try my best to get back to you!
+                    I&apos;m always open to connecting! Whether you have an opportunity to discuss, a question, or just want to say hi, feel free to reach out, and I&apos;ll do my best to get back to you!
                 </p>
 
                 <div className="space-y-8">
